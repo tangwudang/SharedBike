b3ccf1aea3f4108935d96ad228266d491c2bdbdf
tangwudang <wudang_tang@163.com>
2019-01-14 21:06:54 +0800

    fix bug

--- app/src/main/java/com/lishu/bike/widget/FenceDetailDialog.java
+++ app/src/main/java/com/lishu/bike/widget/FenceDetailDialog.java
@@ -8,15 +8,18 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.lishu.bike.R;
 import com.lishu.bike.activity.FenceDetailActivity;
 import com.lishu.bike.model.FenceDetailModel;
+import com.lishu.bike.utils.TimeUtil;
 
 public class FenceDetailDialog extends AlertDialog {
     private AlertDialog alertDialog;
     private Context mContext;
+    private LinearLayout fenceNameLayout, installAddressLayout, remarkLayout, longitudeLayout, installTimeLayout, streetNameLayout, amountLayout, versionLayout;
     private TextView fenceName, installAddress, remark, longitude, installTime, streetName, amount, version;
     private Button detailMapButton;
 
@@ -47,18 +50,76 @@ public class FenceDetailDialog extends AlertDialog {
         streetName = window.findViewById(R.id.streetName);
         amount = window.findViewById(R.id.amount);
         version = window.findViewById(R.id.version);
+        fenceNameLayout = window.findViewById(R.id.fenceName_layout);
+        installAddressLayout = window.findViewById(R.id.installAddress_layout);
+        remarkLayout = window.findViewById(R.id.remark_layout);
+        longitudeLayout = window.findViewById(R.id.longitude_layout);
+        installTimeLayout = window.findViewById(R.id.installTime_layout);
+        streetNameLayout = window.findViewById(R.id.streetName_layout);
+        amountLayout = window.findViewById(R.id.amount_layout);
+        versionLayout = window.findViewById(R.id.version_layout);
         detailMapButton = window.findViewById(R.id.gis_map_button);
     }
 
     public void setDialogData(final FenceDetailModel fenceDetailModel){
-        fenceName.setText(fenceDetailModel.getFenceName());
-        installAddress.setText(fenceDetailModel.getInstallAddress());
-        remark.setText(fenceDetailModel.getRemark());
-        longitude.setText("纬度" + fenceDetailModel.getLatitude() + "经度" + fenceDetailModel.getLongitude());
-        installTime.setText(fenceDetailModel.getInstallTime());
-        streetName.setText(fenceDetailModel.getStreetName());
-        amount.setText("" + fenceDetailModel.getAmount());
-        version.setText(fenceDetailModel.getVersion());
+        String fenceNameV = fenceDetailModel.getFenceName();
+        String installAddressV = fenceDetailModel.getInstallAddress();
+        String remarkV = fenceDetailModel.getRemark();
+        String latitudeV = fenceDetailModel.getLatitude();
+        String longitudeV = fenceDetailModel.getLongitude();
+        String installTimeV = fenceDetailModel.getInstallTime();
+        String streetNameV = fenceDetailModel.getStreetName();
+        String amountV = "" + fenceDetailModel.getAmount();
+        String versionV = fenceDetailModel.getVersion();
+
+        if(TextUtils.isEmpty(fenceNameV)){
+            fenceNameLayout.setVisibility(View.GONE);
+        }else{
+            fenceNameLayout.setVisibility(View.VISIBLE);
+            fenceName.setText(fenceNameV);
+        }
+        if(TextUtils.isEmpty(installAddressV)){
+            installAddressLayout.setVisibility(View.GONE);
+        }else{
+            installAddressLayout.setVisibility(View.VISIBLE);
+            installAddress.setText(installAddressV);
+        }
+        if(TextUtils.isEmpty(remarkV)){
+            remarkLayout.setVisibility(View.GONE);
+        }else{
+            remarkLayout.setVisibility(View.VISIBLE);
+            remark.setText(remarkV);
+        }
+        if(TextUtils.isEmpty(latitudeV) && TextUtils.isEmpty(longitudeV)){
+            longitudeLayout.setVisibility(View.GONE);
+        }else{
+            longitudeLayout.setVisibility(View.VISIBLE);
+            longitude.setText("纬度" + latitudeV + "，经度" + longitudeV);
+        }
+        if(TextUtils.isEmpty(installTimeV)){
+            installTimeLayout.setVisibility(View.GONE);
+        }else{
+            installTimeLayout.setVisibility(View.VISIBLE);
+            installTime.setText(TimeUtil.getMessageTime(installTimeV));
+        }
+        if(TextUtils.isEmpty(streetNameV)){
+            streetNameLayout.setVisibility(View.GONE);
+        }else{
+            streetNameLayout.setVisibility(View.VISIBLE);
+            streetName.setText(streetNameV);
+        }
+        if(TextUtils.isEmpty(amountV)){
+            amountLayout.setVisibility(View.GONE);
+        }else{
+            amountLayout.setVisibility(View.VISIBLE);
+            amount.setText(amountV);
+        }
+        if(TextUtils.isEmpty(versionV)){
+            versionLayout.setVisibility(View.GONE);
+        }else{
+            versionLayout.setVisibility(View.VISIBLE);
+            version.setText(versionV);
+        }
         detailMapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
